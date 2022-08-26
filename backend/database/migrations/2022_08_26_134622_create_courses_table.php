d2c84611e2745791d6c8a29025eef5396f8fa4c3
Harold Levid <hlmarin2003@gmail.com>
2022-08-26 09:28:16 -0500

    migraciones

---
+++ backend/database/migrations/2022_08_26_134622_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->integer('weeks');
+            $table->float('enrall_cost', 8, 2 );
+            $table->string('minimun_skill', 50);
+            $table->foreignId('bootcamp_id')->constrained();
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
